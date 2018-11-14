b42925cc3df9e52952cb5227e30aea2ca17a9161
Atlas667 <josue.nadal@upr.edu>
2018-11-14 16:57:19 -0400

    something

--- index.php
+++ index.php
@@ -18,25 +18,9 @@
         	<?php
         		$menuList = ["Inicio", "Servicios", "Productos", "Contactanos", "Log in"];
         		foreach($menuList as $s){
-        			echo '<li class="nav-item"><a class="nav-link" href="servicios.html">' . $s . '</a></li>';
+        			echo '<li class="nav-item"><a class="nav-link" href="?p=' . $s . '">' . $s . '</a></li>';
         		}
         	?>
-
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="servicios.html">Servicios</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="productos.html">Productos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contactanos.html">Contactanos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="login.html">Log in</a>
-          </li>
           <li class="nav-item navbar-right">
             <a class="nav-link" href="#">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
