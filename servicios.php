f907c70a5c58d9d39e34a8c54191dbf791ff58fc
Malcolm <malcolm.rose@upr.edu>
2018-12-07 16:54:07 -0500

    bhjvghcfgkv

--- servicios.php
+++ servicios.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
  
     <head>
         <title>Millenium Tint & Design</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="css/style.css">
+        
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     
     </head>
     
@@ -13,67 +17,117 @@
       include "req/nav.php";
     ?>
     
-    <body style="margin-top: 5%">
+     <body>
+
+   <main role="main">
+
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <h1 class="jumbotron-heading">Servicio</h1>
+        <p class="lead text-muted">Nuestro servicios de nuestra compañia.</p>
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="first-slide" src="pics/banner.jpg" alt="First slide">
+            <div class="container">
+              <div class="carousel-caption text-left">
         
-        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-            <div class="col-md-5 p-lg-5 mx-auto my-5">
-              <h1 class="display-4 font-weight-normal">Servicios</h1>
-              <p class="lead font-weight-normal">Ofrecemos servicios de calidad a un precio accesible.</p>
-              <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+               
+              </div>
             </div>
-            <div class="product-device shadow-sm d-none d-md-block"></div>
-            <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+          </div>
+          <div class="carousel-item">
+            <img class="second-slide" src="pics/banner2.jpg" alt="Second slide">
+            <div class="container">
+              <div class="carousel-caption">
+            
+                <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+
+
+      <!-- Marketing messaging and featurettes
+      ================================================== -->
+      <!-- Wrap the rest of the page in another container to center all the content. -->
+
+      <div class="container marketing">
+
+
+
+        <!-- START THE FEATURETTES -->
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">Tinte Industriales. <span class="text-muted"> Mas belleza para tu auto.</span></h2>
+            <p class="lead">Nuestro servicio principal es el de tintes industrales para todo tipo de ventana para su auto.</p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" rounded src="pics/tinte.jpg" alt="Generic placeholder image">
+          </div>
         </div>
 
-        <div class="tab-content">
-          <div class="tab-pane fade show active" id="home" role="tabpanel">
-            <div class="my-3 py-3">
-                <h2 class="display-5">Lavado Sencillo</h2>
-                <p class="lead">And an even wittier subheading.</p>
-              </div>
-              <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7 order-md-2">
+            <h2 class="featurette-heading"> Arreglos de Cristales <span class="text-muted">Oh Si, asi de asombroso.</span></h2>
+            <p class="lead">Arreglamos todo servicio de cristales de su automovil .</p>
           </div>
-          <div class="tab-pane fade" id="profile" role="tabpanel">
-            <div class="my-3 py-3">
-                <h2 class="display-5">Lavado Sencillo</h2>
-                <p class="lead">And an even wittier subheading.</p>
-              </div>
-              <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-          </div>
-          <div class="tab-pane fade" id="messages" role="tabpanel">
-            <div class="my-3 py-3">
-                <h2 class="display-5">Lavado Sencillo</h2>
-                <p class="lead">And an even wittier subheading.</p>
-              </div>
-              <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-          </div>
-          <div class="tab-pane fade" id="settings" role="tabpanel">
-            <div class="my-3 py-3">
-                <h2 class="display-5">Lavado Sencillo</h2>
-                <p class="lead">And an even wittier subheading.</p>
-              </div>
-              <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+          <div class="col-md-5 order-md-1">
+            <img class="featurette-image rounded img-fluid mx-auto" rounded src="pics/rompe_cristales.jpg" alt="Generic placeholder image">
           </div>
         </div>
 
-        <script>
-          $('#myList a').on('click', function (e) {
-          e.preventDefault()
-          $(this).tab('show')
-          })
-        </script>
-        
-        <!-- Footer -->
-        <?php
-          include("req/footer.php");
-        ?>
-        
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    
-    </body>
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">Limpieza de Focos. <span class="text-muted">Te va a quedar espectacular.</span></h2>
+            <p class="lead">Limpieza de focos para asi alumbrar mejpor y para no perder la belleza de tus focos.</p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" rounded src="pics/Arreglar_Focos.jpg" alt="Generic placeholder image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <!-- /END THE FEATURETTES -->
+
+      </div><!-- /.container -->
 
 
+      <!-- FOOTER -->
+      <footer class="container">
+        <p class="float-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+    </main>
 
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+  </body>
 </html>
